3248436f28dd2b51509fff9cd23dadad5dff1b07
rcnfbcspdr <rcnfbcspdr@gmail.com>
2024-02-26 17:36:54 +0800

    fixed sizing
    fixed img, data container and meter container sizing on big screens

--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@ function setZoom() {
 	document.body.style.zoom = ""+window.innerHeight/7.3+"%";
 }
 window.onload = setZoom;
+window.onresize =setZoom;
 
 function formatTimeDate(timedate) {
     const year = timedate.getFullYear();
