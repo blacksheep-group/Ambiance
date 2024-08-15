25fa70cb85f93b96695b75b9480b272c0361e39a
Marvin James A. Erosa <104268552+marvinjameserosa@users.noreply.github.com>
2024-08-15 16:13:44 +0800

    print exception in the serial monitor

--- sketch-files/all-sensors/all-sensors.ino
+++ sketch-files/all-sensors/all-sensors.ino
@@ -23,65 +23,60 @@ uint32_t getAbsoluteHumidity(float temperature, float humidity) {
 
 void setup() {
   Serial.begin(9600);
-  LoRa.setPins(CS, RST, G); // Set pins for LoRa
-
-  int16_t ret;
-  uint8_t auto_clean_days = 4;
-  uint32_t auto_clean;
-  sensirion_i2c_init();
-
   while (!Serial) {
   }
+
+  LoRa.setPins(CS, RST, G); // Set pins for LoRa
+
   if (!bme.begin()) {
     Serial.println("Failed to initialize BME680 sensor!");
-    while (1); // Infinite loop
   } 
   if (!sgp.begin()){
     Serial.println("Failed to initialize SGP30 sensor!");
-    while (1); // Infinite loop
   } 
-  if (!LoRa.begin(433E6)) {   // initialize radio at 433 MHz
-    Serial.println("Failed to initialize LoRa radio!");
-    while (true); // Infinite loop
-  } else {
-    LoRa.setSyncWord(0xF3);
-    // Set up oversampling and filter initialization
-    bme.setTemperatureOversampling(BME680_OS_8X);
-    bme.setHumidityOversampling(BME680_OS_2X);
-    bme.setPressureOversampling(BME680_OS_4X);
-    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-    bme.setGasHeater(320, 150); // 320°C for 150 ms
 
-    ret = sps30_set_fan_auto_cleaning_interval_days(auto_clean_days);
-    if (ret) {
-      Serial.print("error setting the auto-clean interval: ");
-      Serial.println(ret);
-    }
-    ret = sps30_start_measurement();
-    if (ret < 0) {
-      Serial.println("error starting measurement");
-    }
+  sensirion_i2c_init();
+
+  while(!LoRa.begin(433E6)) {   // initialize radio at 433 MHz
+    Serial.println("Failed to initialize LoRa radio!");
   }
+  LoRa.setSyncWord(0xF3);
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320°C for 150 ms
+
 }
 
 
 void loop() {
-  // Error handling for BME680 sensor
-  if (!bme.performReading()) {
-    Serial.println("Failed to perform BME680 reading!");
-    delay(1000);
-    return;
-  }
-
-  // Error handling for SGP30 sensor
-  if (!sgp.IAQmeasure()) {
-    Serial.println("Failed to perform SGP30 reading!");
-    delay(1000);
-    return;
-  }
-
-  // Sending sensor data via LoRa
   LoRa.beginPacket();
+
+  int16_t ret;
+  uint8_t auto_clean_days = 4;
+  uint32_t auto_clean;
+  ret = sps30_set_fan_auto_cleaning_interval_days(auto_clean_days);
+
+  while(!bme.performReading() || !sgp.IAQmeasure() || ret){
+    
+    if (!bme.performReading()) {
+      Serial.print("exception:");
+      Serial.println("Failed to perform BME680 reading! Please check the sensor connection.");
+      delay(1000);
+      return;
+    } else if (!sgp.IAQmeasure()) {
+      Serial.print("exception:");
+      Serial.println("Failed to perform SGP30 reading! Please check the sensor connection.");
+      delay(1000);
+      return;
+    } else if(ret){
+      Serial.print("exception:");
+      Serial.print("Failed to set the auto-clean SGP30");
+    }
+    
+  }
 
   // Location
   Serial.println("location:CONDOTEL");
@@ -122,20 +117,20 @@ void loop() {
   Serial.println(sgp.eCO2); 
   LoRa.print("eco2:");
   LoRa.print(sgp.eCO2);
-  LoRa.endPacket();
+ 
 
   struct sps30_measurement m;
   char serial[SPS30_MAX_SERIAL_LEN];
   uint16_t data_ready;
-  int16_t ret;
 
   do {
     ret = sps30_read_data_ready(&data_ready);
     if (ret < 0) {
-      Serial.print("error reading data-ready flag: ");
+      Serial.print("exception:");
+      Serial.print("Failed to perform reading SPS30! Data-ready flag: ");
       Serial.println(ret);
     } else if (!data_ready)
-      Serial.print("data not ready, no new measurement available\n");
+      Serial.print("Failed to perform reading SPS30! Please check the sensor connection.");
     else
       break;
     delay(100); 
@@ -148,12 +143,15 @@ void loop() {
     Serial.println(m.nc_1p0);
     Serial.print("pm2:");
     Serial.println(m.nc_2p5);
-    Serial.print("pm10");
+    Serial.print("pm10:");
     Serial.println(m.nc_10p0);
 
     //Serial.print("Typical particle size: ");
     //Serial.println(m.typical_particle_size);
   }
+  LoRa.endPacket();
   delay(1000);
 }
+
+
 
